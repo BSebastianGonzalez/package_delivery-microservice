bf6f1f8ea6f33cee5c411ec69141b2cf50cdf28d
edwind99 <edanduarte1@hotmail.com>
2025-10-21 17:43:48 -0500

    first commit

---
+++ src/main/java/com/example/package_delivering/PackageDeliveringApplication.java
@@ -0,0 +1,13 @@
+package com.example.package_delivering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageDeliveringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageDeliveringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/package_delivering/PackageDeliveringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.package_delivering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageDeliveringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
